f990f9cabd6b51e6ce321d18651bc42562642b09
Yueh-Cheng Chang <austinyuch@users.noreply.github.com>
2022-07-07 04:23:50 +0000

    fix in docker host

--- confs.py
+++ confs.py
@@ -1,11 +1,11 @@
 dic_api = {
-    "site_name":"iekchartsapi", 
-    "description":"IEK Charts",
-    "site_url":"http://localhost:2224",
-    "api_url":"https://",
+    "site_name":"API Algorithms", 
+    "description":"Algorithms Service",
+    "site_url":"http://0.0.0.0:2224",
+    "api_url":"https://apialgorithms.yuehchengchang.repl.co",
     # "version_note":"",
     # "last_update":"2021/08/05",
-    "host": "localhost", #cloud server用0.0.0.0, # windows local不能用 0.0.0.0
+    "host": "0.0.0.0", #cloud server用0.0.0.0, # windows local不能用 0.0.0.0
     "port": 2224, 
     "reload": False, # 正式佈署時用False比較穩定
     "workers": 1, #開multiprocess
