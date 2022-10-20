ef52d826c0342015976a32feb56e18e6964c496b
austinyuch <ycchang.pmp@gmail.com>
2022-10-20 17:50:49 +0800

    feat:修正階層和金額,加入累計下跌百分比

--- algo_pyramid.py
+++ algo_pyramid.py
@@ -14,7 +14,7 @@ def get單位價格差(flt_起始價格:float,
                 int_交易次數:int)->float:
     flt總價格差 = flt_起始價格 - flt最終價格
     print(f'{flt總價格差=}')
-    flt_單位價格差 = abs(flt總價格差) / (int_交易次數 - 1)
+    flt_單位價格差 = abs(flt總價格差) / (int_交易次數 )
     print(f'{flt_單位價格差=}')
     return flt_單位價格差
 
@@ -23,25 +23,52 @@ def getLst買入價格(flt_起始價格:float,
                 int_交易次數:int)->list:
     # 計算買入價格
     lst_買入價格=[]
-    for i in range(int_交易次數 - 1):
+    for i in range(int_交易次數 +1 ):
+        
         flt買入價格 = flt_起始價格 - (flt_單位價格差 * i)
-        lst_買入價格.append(flt買入價格)
+        if flt買入價格==flt_起始價格:
+            continue
+        else:
+            lst_買入價格.append(flt買入價格)
 
     print(f'{lst_買入價格=}')
+
     return lst_買入價格
 
+def get本階投入資金百分比(flt_一組金字塔的加總金額,flt_下單金額)->float:
+    return (flt_下單金額/flt_一組金字塔的加總金額)*100
 
-def etl_percentage(dic_金字塔下單資料:dict,flt_一組金字塔的加總金額:float)->dict:
+def get下跌百分比(flt_起始價格,flt_當前價格)->float:
+    return ((flt_起始價格 - flt_當前價格)/flt_起始價格)*100
+
+def etl_percentage(dic_金字塔下單資料:dict,
+                flt_一組金字塔的加總金額:float,
+                flt_起始價格:float,
+                )->dict:
     
     dic_金字塔下單資料_ret = copy.deepcopy(dic_金字塔下單資料)
-    dic_金字塔下單資料_ret["總金額"] = flt_一組金字塔的加總金額
+    dic_金字塔下單資料_ret["總投入金額"] = flt_一組金字塔的加總金額
+    dic_金字塔下單資料_ret["起始價格"] = flt_起始價格
     flt_累計百分比 = 0
+    flt_累計下跌百分比 = 0
+    flt_最終價格=0
+
     for key,value in dic_金字塔下單資料.items():        
-        dic_金字塔下單資料_ret[key]["百分比"]= (dic_金字塔下單資料_ret[key]["金額"]/dic_金字塔下單資料_ret["總金額"])*100
+        # dic_金字塔下單資料_ret[key]["百分比"]= (dic_金字塔下單資料_ret[key]["金額"]/flt_一組金字塔的加總金額)*100
+        dic_金字塔下單資料_ret[key]["百分比"] = get本階投入資金百分比(flt_一組金字塔的加總金額,dic_金字塔下單資料_ret[key]["金額"])
+
         flt_累計百分比 += dic_金字塔下單資料_ret[key]["百分比"]
         dic_金字塔下單資料_ret[key]["累計百分比"]= flt_累計百分比
+        
+        # 計算累計下跌百分比 : ((起始價格 - 本階價格)/起始價格)*100
+        flt_累計下跌百分比 += get下跌百分比(flt_起始價格,dic_金字塔下單資料_ret[key]['價格'])
+        # flt_累計下跌百分比 += ((flt_起始價格 - dic_金字塔下單資料_ret[key]['價格'])/flt_起始價格)*100
+        dic_金字塔下單資料_ret[key]["累計下跌百分比"]= flt_累計下跌百分比
+        flt_最終價格=dic_金字塔下單資料_ret[key]['價格']
 
-
+    dic_金字塔下單資料_ret["最終價格"] = flt_最終價格
+    dic_金字塔下單資料_ret["累計下跌百分比"] = flt_累計下跌百分比
+    
     return dic_金字塔下單資料_ret
 
 
@@ -64,28 +91,36 @@ def get本階下單資料(lst_買入價格, flt_最小增加數量, round單位�
 
 #====等差金字塔====
 
-def get一組等差金字塔下單資料(int_交易次數:int, 
-                            flt_下單數量等差參數:float, 
-                            flt_起始單位數:float, 
-                            lst_買入價格:list,
-                            flt_最小增加數量:float,
-                            flt_金字塔放大倍數:float=1.0,
-                            round單位數:bool=False)->dict:
+def get一組等差金字塔下單資料(flt_起始價格:float,
+                        int_交易次數:int, 
+                        flt_下單數量等差參數:float, 
+                        flt_起始單位數:float, 
+                        lst_買入價格:list,
+                        flt_最小增加數量:float,
+                        flt_金字塔放大倍數:float=1.0,
+                        round單位數:bool=False)->dict:
     flt_一組金字塔的加總金額 = 0
     dic_單位等差金字塔下單資料 ={}
-    for i in range(int_交易次數 - 1):
+    
+    for i in range(int_交易次數  ):
         
         # 本階金額 = 本階買入價格 * 本階單位數  
         flt_本階單位數 = (flt_起始單位數 + (flt_下單數量等差參數 * i) ) * flt_金字塔放大倍數
        
-        dic_本階下單資料 = get本階下單資料(lst_買入價格, flt_最小增加數量, round單位數, i, flt_本階單位數)
+        dic_本階下單資料 = get本階下單資料(lst_買入價格, 
+                                    flt_最小增加數量, 
+                                    round單位數, 
+                                    i, 
+                                    flt_本階單位數)
 
         階層數 = str(i + 1)
         dic_單位等差金字塔下單資料[階層數] = dic_本階下單資料
         flt_一組金字塔的加總金額 += dic_本階下單資料["金額"]
     
     # 處理各階層佔比    
-    dic_單位等差金字塔下單資料 = etl_percentage(dic_單位等差金字塔下單資料,flt_一組金字塔的加總金額)
+    dic_單位等差金字塔下單資料 = etl_percentage(dic_單位等差金字塔下單資料,
+                                            flt_一組金字塔的加總金額,
+                                            flt_起始價格)
 
     print(f'{dic_單位等差金字塔下單資料=}')
 
@@ -105,11 +140,17 @@ def get買入等差金字塔(flt_總預算:float,
     
     lst_買入價格 = getLst買入價格(flt_起始價格,flt_單位價格差,int_交易次數)
     
-    dic_單位等差金字塔下單資料 = get一組等差金字塔下單資料(int_交易次數, flt_下單數量等差參數, flt_起始單位數, lst_買入價格,flt_最小增加數量)
+    dic_單位等差金字塔下單資料 = get一組等差金字塔下單資料(flt_起始價格,
+                                                int_交易次數, 
+                                                flt_下單數量等差參數, 
+                                                flt_起始單位數, 
+                                                lst_買入價格,
+                                                flt_最小增加數量)
 
-    flt_金字塔放大倍數 = flt_總預算 / dic_單位等差金字塔下單資料["總金額"] 
+    flt_金字塔放大倍數 = flt_總預算 / dic_單位等差金字塔下單資料["總投入金額"] 
 
-    dic_等差金字塔下單資料 = get一組等差金字塔下單資料(int_交易次數, 
+    dic_等差金字塔下單資料 = get一組等差金字塔下單資料(flt_起始價格,
+                                                int_交易次數, 
                                                 flt_下單數量等差參數, 
                                                 flt_起始單位數, 
                                                 lst_買入價格,
@@ -123,16 +164,17 @@ def get買入等差金字塔(flt_總預算:float,
 #====等比金字塔====
 
 
-def get一組等比金字塔下單資料(int_交易次數:int, 
-                            flt_下單數量等比參數:float, 
-                            flt_起始單位數:float, 
-                            lst_買入價格:list,
-                            flt_最小增加數量:float,
-                            flt_金字塔放大倍數:float=1.0,
-                            round單位數:bool=False)->dict:
+def get一組等比金字塔下單資料(flt_起始價格:float,
+                        int_交易次數:int, 
+                        flt_下單數量等比參數:float, 
+                        flt_起始單位數:float, 
+                        lst_買入價格:list,
+                        flt_最小增加數量:float,
+                        flt_金字塔放大倍數:float=1.0,
+                        round單位數:bool=False)->dict:
     flt_一組金字塔的加總金額 = 0
     dic_單位等比金字塔下單資料 ={}
-    for i in range(int_交易次數 - 1):
+    for i in range(int_交易次數 ):
         
         # 本階金額 = 本階買入價格 * 本階單位數  
         flt_本階單位數 = (flt_起始單位數 * (flt_下單數量等比參數 ** i) ) * flt_金字塔放大倍數
@@ -145,7 +187,9 @@ def get一組等比金字塔下單資料(int_交易次數:int,
     # 處理各階層佔比
     
     
-    dic_單位等比金字塔下單資料= etl_percentage(dic_單位等比金字塔下單資料,flt_一組金字塔的加總金額)
+    dic_單位等比金字塔下單資料= etl_percentage(dic_單位等比金字塔下單資料,
+                                            flt_一組金字塔的加總金額,
+                                            flt_起始價格)
 
     print(f'{dic_單位等比金字塔下單資料=}')
 
@@ -164,11 +208,17 @@ def get買入等比金字塔(flt_總預算:float,
     
     lst_買入價格 = getLst買入價格(flt_起始價格,flt_單位價格差,int_交易次數)
     
-    dic_單位等比金字塔下單資料 = get一組等比金字塔下單資料(int_交易次數, flt_下單數量等比參數, flt_起始單位數, lst_買入價格,flt_最小增加數量)
+    dic_單位等比金字塔下單資料 = get一組等比金字塔下單資料(flt_起始價格,
+                                                int_交易次數, 
+                                                flt_下單數量等比參數, 
+                                                flt_起始單位數, 
+                                                lst_買入價格,
+                                                flt_最小增加數量)
 
-    flt_金字塔放大倍數 = flt_總預算 / dic_單位等比金字塔下單資料["總金額"] 
+    flt_金字塔放大倍數 = flt_總預算 / dic_單位等比金字塔下單資料["總投入金額"] 
 
-    dic_等比金字塔下單資料 = get一組等比金字塔下單資料(int_交易次數, 
+    dic_等比金字塔下單資料 = get一組等比金字塔下單資料(flt_起始價格,
+                                                int_交易次數, 
                                                 flt_下單數量等比參數, 
                                                 flt_起始單位數, 
                                                 lst_買入價格,
@@ -180,7 +230,7 @@ def get買入等比金字塔(flt_總預算:float,
     return dic_等比金字塔下單資料
 
 if __name__ == "__main__":
-
+    lst_買入價格 = getLst買入價格(400,5,5)
     flt_總預算 = float(100000) #float(input("請輸入總預算："))
     flt_起始價格 = float(440) #float(input("請輸入起始金額："))
     flt最終價格 = float(380) #float(input("請輸入最終金額："))
